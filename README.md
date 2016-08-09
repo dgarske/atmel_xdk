c6f77bf682e59038d329d0a8738876133feb171b
David Garske <david@wolfssl.com>
2016-08-08 17:03:26 -0700

    This atmel_xdk repo has been merged into: https://github.com/dgarske/atmel

---
+++ README.md
@@ -0,0 +1,2 @@
+This atmel_xdk repo has been merged into:
+https://github.com/dgarske/atmel
--- sam0/applications/wolfcrypt/samd21j18a_samd21_xplained_pro/gcc/debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-arm-none-eabi-gdb wolfcrypt_flash.elf -ex 'target remote | openocd -c "gdb_port pipe;" -f ../../../../utils/openocd/atmel_samd21_xplained_pro.cfg'
-
